4855c27a699af3e702e9f379138eeab7610d5635
Chathushka Hasaranga <chathushka03hasaranga@gmail.com>
2025-08-26 18:33:34 +0530

    login_done
    login page ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter,Route,Routes } from "react-router-dom"
 import AdminDashboard from "./pages/admin/adminDashboard"
+import Register from "./pages/user/register"
 function App() {
  return(
     <BrowserRouter>
     <Routes path='/*'>
       <Route path="/admin" element={<AdminDashboard></AdminDashboard>}></Route>
+      <Route path="/register" element={<Register></Register>}></Route>
     </Routes>
 
   </BrowserRouter>
