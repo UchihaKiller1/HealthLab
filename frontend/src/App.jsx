f26ea82fa5f213378ab16081b0aec994d18d58e3
UchihaKiller1 <amindurajamuni@gmail.com>
2025-08-26 15:42:02 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter,Route,Routes } from "react-router-dom"
+
+function App() {
+ return(
+    <BrowserRouter>
+    <Routes  path='/*'>
+      <Route></Route>
+    </Routes>
+
+  </BrowserRouter>
+
+
+ )
+}
+
+export default App
