c6b118377a830ab39ef4fc2abc5cb81b85d89f6b
Chathushka Hasaranga <chathushka03hasaranga@gmail.com>
2025-08-27 17:17:54 +0530

    about us new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter,Route,Routes } from "react-router-dom"
 import AdminDashboard from "./pages/admin/adminDashboard"
 import Register from "./pages/user/register"
 import Login from "./pages/user/login"
+import AboutUs from "./pages/user/aboutUs"
 function App() {
  return(
     <BrowserRouter>
@@ -9,6 +10,7 @@ function App() {
       <Route path="/admin" element={<AdminDashboard></AdminDashboard>}></Route>
       <Route path="/register" element={<Register></Register>}></Route>
       <Route path="/login" element={<Login></Login>}></Route>
+      <Route path="/about" element={<AboutUs></AboutUs>}></Route>
     </Routes>
 
   </BrowserRouter>
